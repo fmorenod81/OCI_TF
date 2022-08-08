a688eb892d982c6b2ea4b135af5f03067219a6ae
Francisco Moreno <f-moreno@javeriana.edu.co>
2022-08-08 13:50:19 -0500

    :memo: Actualizando por compartments

--- CLI/sample.bat
+++ CLI/sample.bat
@@ -1,4 +1,6 @@
-oci iam compartment list|jq ".data[] | select( .name | contains(\"TestTerraform\"))"|jq .\"id\" > TempFile
+oci iam compartment list|jq ".data[] | select( .name | contains(\"Francisco\"))"|jq .\"id\" > TempFile
+set /p T= <TempFile
+oci iam compartment list -c %T%|jq ".data[] | select( .name | contains(\"DevOps\"))"|jq .\"id\" > TempFile
 set /p T= <TempFile
 oci compute instance list -c %T%
 del TempFile
